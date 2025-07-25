4b9a3ccc296906fe56d495d74076e87427f92e32
BitBravo <jsdev63@yahoo.com>
2025-07-25 07:22:02 -0700

    Update index.js

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ const wss = new WebSocket.Server({ server });
 const port = process.env.PORT || 3000;
 
 mongoose
-  .connect("mongodb://127.0.0.1:27017/emails")
+  .connect(process.env.DB_URL || "mongodb://127.0.0.1:27017/emails")
   .then(() => console.log("MongoDB connected"))
   .catch((err) => console.log("Error connecting to MongoDB:", err));
 
